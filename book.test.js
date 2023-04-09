f0f1e073a0bfd47c61feb4accf16b80309fc08b4
fatimabatool <fatima.batool@xloopdigital.com>
2023-04-09 09:32:58 -0700

    test 1 failed

---
+++ book.test.js
@@ -0,0 +1,9 @@
+const {ReadingArray} = require('./book')
+test("ReadingArray", () => {
+    //SetUp
+    const arr = new ReadingArray();
+    //Execute
+    const result = arr.numRead();
+    //Assert
+    expect(result).toBe(0)
+} )
