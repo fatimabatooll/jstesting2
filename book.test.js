1798e63ac684b06d624d446f8219df692689a76d
fatimabatool <fatima.batool@xloopdigital.com>
2023-04-09 09:55:30 -0700

    test 2 failed

--- book.test.js
+++ book.test.js
@@ -7,3 +7,14 @@ test("ReadingArray", () => {
     //Assert
     expect(result).toBe(0)
 } )
+test ("addBook", () => {
+    //SetUP
+    const arr = new ReadingArray();
+    const book = new Book("Martian","Davidoff",362,2004);
+    arr.addBook(book, "sep 1", 5);
+    //Execute
+    const result = arr.numRead();
+    //assert
+    expect(result).toBe(1);
+    
+})   
